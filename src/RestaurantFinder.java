6457b1befc294c0ab3c6bf5cccf19cca5b5828ca
tolgasoykann <tolgasoykan004@gmail.com>
2025-03-31 02:09:20 +0300

    enhancement for data input

--- src/RestaurantFinder.java
+++ src/RestaurantFinder.java
@@ -10,10 +10,24 @@ public class RestaurantFinder {
         categoryList.addCategory("Chinese");
         categoryList.addCategory("Halal");
         categoryList.addCategory("Vegan");
+        // restaurant data
+        Restaurant[] italianRestaurants = {
+                new Restaurant("Nostra Casa", "Küçük Ayasofya Mh. Mustafa Paşa Sok. No:42, Sultanahmet", 4, true, false),
+                new Restaurant("Fiore Pizzeria", "Miralay Sefikbey Sokak No:9/A Gümüşsuyu", 4, true, false),
+                new Restaurant("Paps Italian", "Fransız Geçidi, Karaköy", 3, true, false),
+                new Restaurant("Elio's", "Apartment or Condo, Istanbul", 4, true, false),
+                new Restaurant("Eataly", "Zorlu Center, Levazım Mah. Koru Sok. No:2, Beşiktaş", 4, true, false),
+                new Restaurant("Da Mario", "Nispetiye Cad. Dilhayat Sok. No:7 Etiler, Beşiktaş", 4, true, false),
+                new Restaurant("La Scarpetta", "Etiler Mh. Dilhayat Sk. No:16, Beşiktaş", 4, true, false),
+                new Restaurant("Paper Moon", "Kültür Mah. Nisbetiye Cad. No:54, Beşiktaş", 5, true, false),
+                new Restaurant("Gina", "Esentepe Mah. Büyükdere Cad. No:185, Şişli", 4, true, false)
+        };
+        // adding data by category
+        for (Restaurant r : italianRestaurants) {
+            categoryList.getCategory("Italian").bst.insert(r);
+        }
 
         // Adding sample restaurants
-        categoryList.getCategory("Italian").bst.insert(new Restaurant("Luigi's", "New York", 4.5, true, false));
-        categoryList.getCategory("Italian").bst.insert(new Restaurant("Pasta House", "Los Angeles", 4.2, false, true));
         categoryList.getCategory("Chinese").bst.insert(new Restaurant("Golden Dragon", "San Francisco", 4.7, true, false));
         categoryList.getCategory("Halal").bst.insert(new Restaurant("Halal Express", "Chicago", 4.3, false, true));
         categoryList.getCategory("Vegan").bst.insert(new Restaurant("VeganHouse","Istanbul",4.1,false,true));
